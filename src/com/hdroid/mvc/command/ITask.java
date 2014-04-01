bfc9e51d6141c2c001cb988f901a90ef940e0490
yuanwenfei <yuanwenfei@7500.com.cn>
2014-04-01 18:38:21 +0800

    ssss

--- src/com/hdroid/mvc/command/ITask.java
+++ src/com/hdroid/mvc/command/ITask.java
@@ -1,19 +1,19 @@
 package com.hdroid.mvc.command;
 
 public interface ITask {
-	TARequest getRequest();
-
-	void setRequest(TARequest request);
-
-	TAResponse getResponse();
-
-	void setResponse(TAResponse response);
+//	TARequest getRequest();
+//
+//	void setRequest(TARequest request);
+//
+//	TAResponse getResponse();
+//
+//	void setResponse(TAResponse response);
 
 	void execute();
 
-	TAIResponseListener getResponseListener();
-
-	void setResponseListener(TAIResponseListener listener);
+//	TAIResponseListener getResponseListener();
+//
+//	void setResponseListener(TAIResponseListener listener);
 
 	void setTerminated(boolean terminated);
 	boolean isTerminated();
